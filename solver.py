0f5d20d4f168e0ed3f98ab49c03f899a6d219f43
Nils-Erik Schütte <nilserik@uni-bremen.de>
2020-09-12 14:32:30 +0200

    Implement routines into main() function
    Add routine to read out schrodinger.inp and assign constants to
    the informations

--- solver.py
+++ solver.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+
+import calculus
+
+
+_DIRECTORY = 'files'
+_FILE = 'schrodinger.inp'
+
+
+def main():
+    """Main function to solve the one dimensiional time independent schrodinger
+    equation."""
+    # Read out the informations of the schrodinger.inp file.
+    inp_data = calculus.io.read_schrodinger(_DIRECTORY, _FILE)
+    _MASS = inp_data["_MASS"]
+    _XMIN = float(inp_data["plot_set"][0])
+    _XMAX = float(inp_data["plot_set"][1])
+    _NPOINT = int(inp_data["plot_set"][2])
+    _MIN_EV = int(inp_data["evalues"][0])
+    _MAX_EV = int(inp_data["evalues"][1])
+    _REG_TYPE = inp_data["regression"]
+    _INTERPOLATE_NR = int(inp_data["interpolate_nr"])
+    _POT = inp_data['pot']
+
+
+if __name__ == '__main__':
+    main()
