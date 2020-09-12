d2bf6c316b8314d1b043b654d255f3a342b924ad
Nils-Erik Schütte <nilserik@uni-bremen.de>
2020-09-12 06:20:55 +0200

    Add function calculating energy of inf square well

--- test_solver.py
+++ test_solver.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
 
+import numpy as np
+
+
+def energy_inf_square_well(EVmax, xmin, xmax, mass):
+    energy = np.zeros((EVmax, ), dtype=float)
+    for nn in range(EVmax):
+        energy[nn] = np.pi**2 * nn**2 / (2 * mass * (xmin - xmax)**2)
+    return energy
