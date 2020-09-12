b301ae9c76614907599b9f64067e8a26197c72c5
Jan-Luca Hubald <janluca@uni-bremen.de>
2020-09-12 18:31:21 +0200

    Add filereading routines

--- calculus/file_io.py
+++ calculus/file_io.py
@@ -50,5 +50,55 @@ def write_result(directory, fname, array):
     """
     np.savetxt(os.path.join(directory, fname), array)
 
-#test
 
+def read_files(filepath):
+    '''reads four different files and converts them into arrays
+
+    Args:
+        filepath: Filepath of , `energies.dat`, `expvalues.dat, `potential.dat`
+        and `wavefuncs.dat`
+
+    Returns:
+        endata: array containing energies of their corresponding eigenstates
+        expxdata: array containing expected values for x and their
+        uncerainnities
+        potdata: array containing potentials and corresponding x-values
+        wfdata: array containing eigenstates and corresponding x-values
+    '''
+    enfile = os.path.join(filepath, "energies.dat")
+    expxfile = os.path.join(filepath, "expvalues.dat")
+    potfile = os.path.join(filepath, "potential.dat")
+    wffile = os.path.join(filepath, "wavefuncs.dat")
+
+    endata = np.loadtxt(enfile)
+    expxdata = np.loadtxt(expxfile)
+    potdata = np.loadtxt(potfile)
+    wfdata = np.loadtxt(wffile)
+
+    return endata, expxdata, potdata, wfdata
+
+
+def create_files(filepath, endata, expxdata, potdata, wfdata):
+    '''
+    Creates files containing the energies (`energies.dat`), expected values for
+    x and thier uncertainities (`expvalues.dat`), the potentials and their
+    corresponding x-values (`potential.dat`) and the eigenstates with their
+    corresponding x_values (`wavefuncs.dat`)
+
+    Args:
+        filepath: Filepath of the destination, in which the files should be
+        saved
+        endata: array containing data dedicated to `energies.dat`
+        expxdata: array containing data dedicated to `expvalues.dat`
+        potdata: array containing data dedicated to `potential.dat`
+        wfdata: array containing data dedicated to `wavefuncs.dat`
+    '''
+    enfile = os.path.join(filepath, "energies.dat")
+    expxfile = os.path.join(filepath, "expvalues.dat")
+    potfile = os.path.join(filepath, "potential.dat")
+    wffile = os.path.join(filepath, "wavefuncs.dat")
+
+    np.savetxt(enfile, endata)
+    np.savetxt(expxfile, expxdata)
+    np.savetxt(potfile, potdata)
+    np.savetxt(wffile, wfdata)
