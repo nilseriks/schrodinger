dbf537bd9be1a0db2bbb2f17069835f6e406f4cd
Nils-Erik Schütte <nilserik@uni-bremen.de>
2020-09-23 06:54:18 +0200

    Change plot routine so it can show only 1 eigenval

--- calculus/plot.py
+++ calculus/plot.py
@@ -57,9 +57,10 @@ def pot_plot(xmin, xmax, min_ev, max_ev, energy, evec, pot, xplot, ydiff, expx,
     atol = 0.05 * ydiff
     rtol = 0.05 * ydiff
     ymin = np.amin(pot) - 0.05 * ydiff
-    _max_scale = _scale_plot(min_ev, max_ev, energy, evec, max_ev - 1, rtol,
-                             atol)
-    ymax = energy[max_ev - 1] + np.amax(_max_scale * evec[:, max_ev - 1]) + 0.05 * ydiff
+    max_scale = _scale_plot(min_ev, max_ev, energy, evec, max_ev - 1, rtol,
+                            atol)
+    ymax = (energy[max_ev - 1] + np.amax(max_scale * evec[:, max_ev - 1]) +
+            0.05 * ydiff)
 
     plt.figure(figsize=(9, 6), dpi=80)
 
@@ -98,6 +99,83 @@ def pot_plot(xmin, xmax, min_ev, max_ev, energy, evec, pot, xplot, ydiff, expx,
         plt.hlines(energy[ii], xmin, xmax, color='lightgray', linewidth=2.5,
                    zorder=1)
         plt.plot(unc[ii], energy[ii], marker='+', color='magenta',
+                 markersize=17, markeredgewidth=1.85, zorder=2)
+    plt.xlim(0, np.amax(unc) + 0.1 * np.amax(unc))
+    plt.ylim(ymin, ymax)
+    plt.yticks([])
+    plt.xticks(fontsize=14)
+    plt.title('sigma x', fontsize=16)
+    plt.xlabel('[Bohr]', fontsize=16)
+
+    ax = plt.gca()
+    ax.spines['top'].set_linewidth(1.2)
+    ax.spines['right'].set_linewidth(1.2)
+    ax.spines['bottom'].set_linewidth(1.2)
+    ax.spines['left'].set_linewidth(1.2)
+
+    plt.show()
+
+
+def pot_plot2(xmin, xmax, min_ev, max_ev, energy, evec, pot, xplot, ydiff,
+              expx, unc):
+    """Creates a graphical plot. It shows the potential, the eigenvalues, the
+    wavefunctions, the expected values of the position of the particle. And
+    within a second plot it shows the uncertainty of the expected position.
+
+    Args:
+        xmin: Lower bound of the x values.
+        xmax: Upper bound of the x values.
+        min_ev: Lower bound of the eigenvalues which should be visualized.
+        max_ev: Upper bound of the eigenvalues which should be visualized.
+        energy: Array of eigenvalues.
+        evec: Array containing the wavefunctions as column vectors.
+        pot: Interpolation of the potential at the xplot values.
+        xplot: Values were the potential is defined.
+        ydiff: Absolute difference between the lowest pot-value and the highest
+               eigenvalue.
+        expx: Expected values of the position.
+        unc: Uncertainty of the position.
+    """
+    scale = 0.4 * abs(energy - np.amin(pot)) * 1 / np.amax(abs(evec[:, 0]))
+    ymin = energy - np.amax(scale * evec[:, 0]) + 0.05 * ydiff
+    ymax = energy + np.amax(scale * evec[:, 0]) + 0.05 * ydiff
+
+    plt.figure(figsize=(9, 6), dpi=80)
+
+    plt.subplot(1, 2, 1)
+    plt.xlim(xmin - 0.05 * abs(xmin), xmax + 0.05 * xmax)
+    plt.ylim(ymin, ymax)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.title('Potential, eigenstates, <x>', fontsize=16)
+    plt.xlabel('x [Bohr]', fontsize=16)
+    plt.ylabel('Energy [Hartree]', fontsize=16)
+
+    ax = plt.gca()
+    ax.spines['top'].set_linewidth(1.2)
+    ax.spines['right'].set_linewidth(1.2)
+    ax.spines['bottom'].set_linewidth(1.2)
+    ax.spines['left'].set_linewidth(1.2)
+    ax.xaxis.set_label_position('bottom')
+
+    for ii in range(min_ev - 1, max_ev):
+        if ii % 2 == 0:
+            color = 'blue'
+        else:
+            color = 'red'
+        plt.hlines(energy, xmin, xmax, color='lightgray', linewidth=2.5,
+                   zorder=1)
+        plt.plot(expx, energy, 'x', color='green', markersize=12,
+                 markeredgewidth=1.5, zorder=3)
+        plt.plot(xplot, scale * evec[:, ii] + energy, color=color,
+                 linewidth=2.5, zorder=2)
+    plt.plot(xplot, pot, color='black', linewidth=2, zorder=0)
+
+    plt.subplot(1, 2, 2)
+    for ii in range(min_ev - 1, max_ev):
+        plt.hlines(energy, xmin, xmax, color='lightgray', linewidth=2.5,
+                   zorder=1)
+        plt.plot(unc, energy, marker='+', color='magenta',
                  markersize=17, markeredgewidth=1.85, zorder=2)
     plt.xlim(0, np.amax(unc) + 0.1 * np.amax(unc))
     plt.ylim(ymin, ymax)
